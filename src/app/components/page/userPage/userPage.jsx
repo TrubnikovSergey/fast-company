d7fd87df2b460c1fe36673f0404cb9d939eb9f2c
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2022-09-03 09:01:21 +0300

    home work finished lesson9_2

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -2,26 +2,30 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import api from "../../../api";
 import Qualities from "../../ui/qualities";
-import { useHistory } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
+import EditUserPage from "../editUserPage/editUserPage";
 
 const UserPage = ({ userId }) => {
     const history = useHistory();
+    const { edit } = useParams();
     const [user, setUser] = useState();
     useEffect(() => {
         api.users.getById(userId).then((data) => setUser(data));
     }, []);
     const handleClick = () => {
-        history.push("/users");
+        history.push(`/users/${user._id}/edit`);
     };
     if (user) {
-        return (
+        return edit ? (
+            <EditUserPage />
+        ) : (
             <div>
                 <h1> {user.name}</h1>
                 <h2>Профессия: {user.profession.name}</h2>
                 <Qualities qualities={user.qualities} />
                 <p>completedMeetings: {user.completedMeetings}</p>
                 <h2>Rate: {user.rate}</h2>
-                <button onClick={handleClick}> Все Пользователи</button>
+                <button onClick={handleClick}>Edit user</button>
             </div>
         );
     } else {
